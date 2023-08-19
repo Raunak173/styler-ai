b762cd6ba1edfa7a73890238caf718e901ec2922
Prakhar <ptofficial29@gmail.com>
2023-08-19 12:19:34 +0530

    backend done & tested

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,33 @@
-const express = require("express");
-require("dotenv").config();
-const openai = require("openai");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const {Configuration, OpenAIApi} = require('openai');
+require('dotenv').config();
+const api_key = process.env.OPEN_API_KEY;
+const config = new Configuration({apiKey: `${api_key}`});
+
+const openai = new OpenAIApi(config);
 const app = express();
-const port = 3001;
-// openai.apiKey = process.env.OPEN_API_KEY;
-openai.apiKey = "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";
 
-app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
 
-app.post("/chatgpt", async (req, res) => {
-  const prompt = req.body.prompt;
+app.get('/', (req, res) => {
+    res.send('Styler.AI Backend is running!');
+});
 
-  try {
-    const response = await openai.Completion.create({
-      engine: "text-davinci-002",
-      prompt: prompt,
-      max_tokens: 50,
-      n: 1,
-      stop: null,
-      temperature: 0.5,
+app.post('/api/generate', async (req, res) => {
+    const {prompt, maxTokens} = req.body;
+    const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt,
+        max_tokens: maxTokens,
+        temperature: 0,
     });
+    console.log(response.data.choices[0].text);
+    res.send(response.data.choices[0].text);
+    }
+);
 
-    res.json({ text: response.choices[0].text });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while processing your request." });
-  }
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
